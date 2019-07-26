8552043745d183fbdf83d58cf00486eeebf8e64d
Wes McElroy <mcelroywes@gmail.com>
2019-07-25 19:04:04 -0700

    Moved errorHandler middleware to its own file

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const errorHandler = require('./errorHandler');
 const app = express();
 
 const morganOption = (NODE_ENV === 'production')
@@ -17,15 +18,6 @@ app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
-app.use(function errorHandler(error, req, res, next) {
-    let response;
-    if (NODE_ENV === 'production') {
-        response = { error : { message: 'server error' } };
-    } else {
-        console.error(error);
-        response = { message: error.message, error };
-    }
-    res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
