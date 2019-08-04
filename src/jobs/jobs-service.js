d3c62cbc5ce50a25a29f2e85a0b278f9f3cf8345
Wes McElroy <mcelroywes@gmail.com>
2019-08-04 13:15:13 -0700

    Modified sanitize job method for returning null interview dates

--- src/jobs/jobs-service.js
+++ src/jobs/jobs-service.js
@@ -18,7 +18,7 @@ const JobsService = {
             job_location: xss(job.job_location),
             salary: xss(job.salary),
             date_applied: dateFns.format(job.date_applied, 'YYYY-MM-DD'),
-            interview_date: dateFns.format(job.interview_date, 'YYYY-MM-DD'),
+            interview_date: job.interview_date ? dateFns.format(job.interview_date, 'YYYY-MM-DD') : null,
             application_status: job.application_status,
             notes: xss(job.notes),
             user_id: job.user_id
